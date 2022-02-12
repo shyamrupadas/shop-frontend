7338fda37468f0e51d19cc4cc4dc1bd6b9885ee3
Comingsoon <comingsoon000@ya.ru>
2022-02-12 03:06:05 +0300

    categories-slice: implement get categories function

---
+++ src/shared/api/categories/getCategories.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { Category } from "shared/types/types";
+
+const getCategories = async (): Promise<Category[]> => {
+  try {
+    const res = await axios.get('https://sbermarket-internship.herokuapp.com/categories');
+    const categories: Category[] = res.data;
+    return categories;
+  }
+  catch(e) {
+    console.log(e);
+    return [];
+  }
+}
+
+export default getCategories;
