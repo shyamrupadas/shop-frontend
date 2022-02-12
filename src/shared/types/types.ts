d67a7d47c0de31893b46512938dbf109e436f9fe
Denis Evlampiev <dev@evlampiev.com>
2022-02-12 14:25:54 +0300

    [issue-6] feature: add base types
    issue: #6

--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,5 +1,10 @@
+/**
+ * Product Types
+ */
+export type ProductId = string;
+
 export type Product = {
-  _id: string;
+  _id: ProductId;
   name: string;
   price: number;
   unitMeasure: string;
@@ -9,10 +14,59 @@ export type Product = {
   category: string;
 }
 
+/**
+ * Categoty Types
+ */
 export type Category = {
   _id: string;
   name: string;
   productsCount: number;
   iconUrl: string;
   products: Product[];
+}
+
+/**
+ * User Types
+ */
+export type UserId = string;
+
+export type User = {
+  _id: UserId;
+  username: string;
+  password: string;
+  avatar: string;
+  is_admin: boolean;
+  is_auth: boolean;
+  cart: CartId;
+}
+
+/**
+ * Cart Types
+ */
+export type CartId = string;
+
+export type Cart = {
+  _id: CartId;
+  user: UserId;
+  productsCount: number;
+  total: number;
+  products: CartProduct[];
+}
+
+/**
+ * Cart Update Types
+ */
+export type CartProductUpdate = {
+  count: number;
+  product: ProductId;
+}
+
+export type CartUpdate = {
+  userId: UserId;
+  products: CartProductUpdate[];
+}
+
+export type CartProduct = {
+  count: number;
+  product: Product;
 }
