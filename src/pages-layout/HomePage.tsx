eb4f6718cd2a2a1658949da35ef30118b7ee9905
Comingsoon <comingsoon000@ya.ru>
2022-02-13 01:04:44 +0300

    server-store: get categories from store, remove useless props

--- src/pages-layout/HomePage.tsx
+++ src/pages-layout/HomePage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Container } from '@mui/material';
 import CategoryList from 'entities/category/ui/CategoryList';
-import { Category } from 'shared/types/types';
+import { useSelector } from 'react-redux';
+import categoriesSelector from 'entities/category/model/categoriesSelectors';
 
-type HomePageProps = {
-  categories: Category[];
-}
-
-const HomePage = ({ categories }: HomePageProps) => {
+const HomePage = () => {
+  const categories = useSelector(categoriesSelector.categories);
+  console.log('render HomePage');
+  
   return (
     <Container maxWidth="lg">
       <CategoryList categories={categories}/>
