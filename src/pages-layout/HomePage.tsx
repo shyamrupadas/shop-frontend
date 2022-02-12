ff9ee96063d0c95e60a6cadaa98dd2ceeb26e1ca
Comingsoon <comingsoon000@ya.ru>
2022-02-12 03:11:11 +0300

    categories-slice: add categories props

--- src/pages-layout/HomePage.tsx
+++ src/pages-layout/HomePage.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Container } from '@mui/material';
 import CategoryList from 'entities/category/ui/CategoryList';
+import { Category } from 'shared/types/types';
 
-const HomePage = () => {
+type HomePageProps = {
+  categories: Category[];
+}
+
+const HomePage = ({ categories }: HomePageProps) => {
   return (
     <Container maxWidth="lg">
-      <CategoryList/>
+      <CategoryList categories={categories}/>
     </Container>
   );
 };
