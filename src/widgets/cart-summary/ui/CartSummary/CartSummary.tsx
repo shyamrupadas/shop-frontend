6e77b8e59b69534cd984d40a43455fc3e8154e57
Denis Evlampiev <dev@evlampiev.com>
2022-02-13 23:10:00 +0300

    [issue-15] feature: include model into CartSummary
    issue: #15

--- src/widgets/cart-summary/ui/CartSummary/CartSummary.tsx
+++ src/widgets/cart-summary/ui/CartSummary/CartSummary.tsx
@@ -1,13 +1,15 @@
 import { Button, Card, CardContent, Typography } from '@mui/material';
+import { cartModel } from 'entities/cart';
 import React from 'react';
+import { useAppSelector } from 'store/store';
 
 export const CartSummary = () => {
+    const totalPrice = useAppSelector(cartModel.selectors.cartTotal);
     return (
         <Card sx={{ display: 'flex' }} style={{ position: 'sticky', top: '10px'}}>
             <CardContent sx={{ flex: '1 0 auto' }}>
-                <Typography variant='h6' component='h2'>Оформление заказа </Typography>
                 <Typography variant='h6' component='h2'>Общая стоимость: </Typography>
-                <Typography variant='h6' component='h2'>12 500 ₽ </Typography>
+                <Typography variant='h6' component='h2'>{totalPrice && `${totalPrice} ₽`}</Typography>
                 <Button color='primary' variant='contained' fullWidth={true}>Оформить заказ</Button>
             </CardContent>
         </Card>
