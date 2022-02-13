298bab8e60498f0d1f5e9cdd408b70da7cad7100
Denis Evlampiev <dev@evlampiev.com>
2022-02-13 22:36:28 +0300

    [issue-15] feature: AddToCart ui
    issue: #15

--- src/features/add-to-cart/ui/AddToCart/AddToCart.tsx
+++ src/features/add-to-cart/ui/AddToCart/AddToCart.tsx
@@ -1,8 +1,29 @@
-/**
- * TODO: Заглушка
- */
+import { Box, ButtonGroup, IconButton, Typography } from "@mui/material";
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
 export  function AddToCart() {
     return (
-        <div>- 0 +</div>
+        <ButtonGroup  aria-label="outlined primary button group">
+            <IconButton color="primary">
+                <RemoveCircleOutlineIcon />
+            </IconButton>
+
+            <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minWidth: '2ch'
+            }}>
+                <Typography sx={{ fontSize: 'default' }} color="text.secondary" component='span'>
+                    0
+                </Typography>
+            </Box>
+
+            <IconButton color="primary">
+                <AddCircleOutlineIcon />
+            </IconButton>
+
+        </ButtonGroup>
     );
 }
