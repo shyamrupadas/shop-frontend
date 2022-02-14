3412dd303f0ba28b2444f95be16c07d633cd82c6
Comingsoon <comingsoon000@ya.ru>
2022-02-14 10:31:42 +0300

    server-store: move selector from HomePage to CategoryList

--- src/pages-layout/HomePage.tsx
+++ src/pages-layout/HomePage.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { Container } from '@mui/material';
 import CategoryList from 'entities/category/ui/CategoryList';
-import { useSelector } from 'react-redux';
-import categoriesSelector from 'entities/category/model/categoriesSelectors';
 
 const HomePage = () => {
-  const categories = useSelector(categoriesSelector.categories);
-  console.log('render HomePage');
-  
   return (
     <Container maxWidth="lg">
-      <CategoryList categories={categories}/>
+      <CategoryList />
     </Container>
   );
 };
