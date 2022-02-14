c862864098ca191dc63c390853ef1d21e739b001
Comingsoon <comingsoon000@ya.ru>
2022-02-14 10:33:55 +0300

    server-store: add EntityState type

--- src/entities/category/model/categoriesSelectors.ts
+++ src/entities/category/model/categoriesSelectors.ts
@@ -1,3 +1,5 @@
+import { EntityState } from "@reduxjs/toolkit";
+import { Category } from "shared/types/types";
 import { RootState } from "store/store";
 import { categoriesEntityAdapter } from "./categoriesSlice";
 
@@ -5,7 +7,7 @@ const {
   selectIds: categoriesIds,
   selectById: categoryById,
   selectAll: categories,
-} = categoriesEntityAdapter.getSelectors((state: RootState) => state.categories);
+} = categoriesEntityAdapter.getSelectors((state: RootState): EntityState<Category> => state.categories);
 
 const categoriesSelector = {
   categoriesIds,
