813950c3070bdf085c81cea59c5c562f2712f2a0
Sergey Maksimov <83498018+shyamrupadas@users.noreply.github.com>
2022-02-15 16:11:35 +0300

    Link to main (#26)
    * add: link to main in Header
    
    * fix: markup
    
    * fix: markup

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material';
 import { SmallCartButton } from 'features/smal-cart-button';
+import Link from 'next/link';
 
 export const Header = () => {
   return (
@@ -8,9 +9,19 @@ export const Header = () => {
       <AppBar position="static">
         <Container maxWidth="lg">
           <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              КлонМаркет
-            </Typography>
+            <Box sx={{ flexGrow: 1 }}>
+              <Link href="/">
+                <a>
+                  <Typography
+                    variant="h6"
+                    component="span"
+                    sx={{ flexGrow: 1 }}
+                  >
+                    КлонМаркет
+                  </Typography>
+                </a>
+              </Link>
+            </Box>
             <SmallCartButton />
           </Toolbar>
         </Container>
