25bd6ba046107ae7e486378adecc493936e3f8b2
Comingsoon <comingsoon000@ya.ru>
2022-02-15 12:56:35 +0300

    homePage-refactor: add adaptive behavoir from width screen

--- src/entities/product/ui/ProductsList.tsx
+++ src/entities/product/ui/ProductsList.tsx
@@ -1,18 +1,37 @@
-import { Stack } from '@mui/material';
-import { Category } from 'shared/types/types';
+import { Box, Stack, Typography } from '@mui/material';
+import { Category, Product } from 'shared/types/types';
 import ProductsItem from './ProductsItem';
 import { AddToCart } from '../../../features/add-to-cart';
+import { useWindowWidth } from 'shared/hooks/useWindowWidth';
 
 type ProductsListProps = {
   category: Category;
 };
 
 const ProductsList = ({ category }: ProductsListProps) => {
+  const width = useWindowWidth();
+
+  let renderingProducts: Product[];
+
+  if (width > 1200) {
+    renderingProducts = category.products.slice(0, 5);
+  } else if (width > 960) {
+    renderingProducts = category.products.slice(0, 4);
+  } else if (width > 720) {
+    renderingProducts = category.products.slice(0, 3);
+  } else if (width > 480) {
+    renderingProducts = category.products.slice(0, 2);
+  } else {
+    renderingProducts = category.products.slice(0, 1);
+  }
+
   return (
-    <section>
-      <h3>{category.name}</h3>
-      <Stack direction="row" spacing={5}>
-        {category.products.map((product) => (
+    <Box>
+      <Typography gutterBottom variant="h4" component="h2">
+        {category.name}
+      </Typography>
+      <Stack direction="row" justifyContent="center" spacing={5}>
+        {renderingProducts.map((product) => (
           <ProductsItem key={product._id} product={product}>
             {/*
                     TODO: AddToCart не должен тут находиться.
@@ -22,7 +41,7 @@ const ProductsList = ({ category }: ProductsListProps) => {
           </ProductsItem>
         ))}
       </Stack>
-    </section>
+    </Box>
   );
 };
 
