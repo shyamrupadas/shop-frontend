376ea238cd5b11bacdf5cdac0e4444692da73b94
Sergey Maksimov <83498018+shyamrupadas@users.noreply.github.com>
2022-02-17 13:46:24 +0300

    add: popover to SmallCardButton (#30)
    * add: popover to SmallCardButton
    
    * add: callback to handlers

--- src/features/smal-cart-button/ui/SmallCartButton/SmallCartButton.tsx
+++ src/features/smal-cart-button/ui/SmallCartButton/SmallCartButton.tsx
@@ -1,22 +1,64 @@
-import { Badge, IconButton } from '@mui/material';
+import { Badge, IconButton, Popover, Typography } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import React from 'react';
+import React, { useCallback } from 'react';
 import Link from 'next/link';
 import { useAppSelector } from 'store/store';
 import { cartModel } from 'entities/cart';
+
+const ProductNameWithErrorList = [
+  'Напиток кокосовый Alpro Barista с соей 1,4% 1 л',
+  'Сухарики Finn Crisp ржаные 200 г',
+];
 
 export const SmallCartButton = () => {
   const cartProductsCount = useAppSelector(
     cartModel.selectors.cartProductsCount,
   );
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null,
+  );
+
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+    },
+    [setAnchorEl],
+  );
+
+  const handleClose = useCallback(() => {
+    setAnchorEl(null);
+  }, [setAnchorEl]);
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
-    <Link href="/cart" passHref>
-      <IconButton aria-label="cart" component="span">
-        <Badge badgeContent={cartProductsCount} color="secondary">
-          <ShoppingCartIcon color={'inherit'} />
-        </Badge>
-      </IconButton>
-    </Link>
+    <>
+      <Link href="/cart" passHref>
+        <IconButton aria-label="cart" component="span" onMouseOut={handleClick}>
+          <Badge badgeContent={cartProductsCount} color="secondary">
+            <ShoppingCartIcon color={'inherit'} />
+          </Badge>
+        </IconButton>
+      </Link>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <Typography sx={{ p: 2 }}>Внимание:</Typography>
+        {ProductNameWithErrorList.map((name: string) => (
+          <Typography key={name} sx={{ p: 1 }}>
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            товара "{name}" доступно только 5 шт
+          </Typography>
+        ))}
+      </Popover>
+    </>
   );
 };
