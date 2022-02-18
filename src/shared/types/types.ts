7e8c4f84ab28d75f63b129682cf0d3d6c0f4d7fe
Comingsoon <comingsoon000@ya.ru>
2022-02-18 21:25:02 +0300

    pagination: add PaginationWrapper type

--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -74,3 +74,15 @@ export type CartUpdate = {
   userId: UserId;
   products: CartProductUpdate[];
 };
+
+/**
+ * PaginationWrapper Type
+ */
+
+export type PaginationWrapper<T> = {
+  data: T;
+  length: number;
+  page: number;
+  limit: number;
+  lastPage: number;
+};
