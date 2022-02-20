eb1349de4977055f16518e8f97c3ccb04ec65730
Comingsoon <comingsoon000@ya.ru>
2022-02-20 16:04:32 +0300

    catalog-update: fix preload, import useProductsLoad hook

--- src/pages/catalog/[pid].page.tsx
+++ src/pages/catalog/[pid].page.tsx
@@ -2,32 +2,17 @@ import React from 'react';
 import MainLayout from '../MainLayout';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { Product } from '../../shared/types';
-import { initStore } from '../../store/store';
-import { getProductsThunk } from '../../entities/product/model/productsThunk';
 import { CatalogPage } from '../../pages-layout/CatalogPage';
+import { getServerSideStore } from 'store';
+import { productsHooks } from 'entities/product/model';
 
-type CatalogProps = {
-  products: Product[];
-};
+export const getServerSideProps = async () => await getServerSideStore();
 
-// Todo: categoryId должно приходить из роутинга
-const categoryId = '6206cc322d75374955d3e9e6';
-
-export const getServerSideProps = async () => {
-  const store = initStore();
-  await store.dispatch(getProductsThunk(categoryId));
-
-  return {
-    props: {
-      preloadedState: store.getState(),
-    },
-  };
-};
-
-const Catalog: NextPage<CatalogProps> = () => {
+const Catalog: NextPage = () => {
   const router = useRouter();
-  const { pid } = router.query;
+  const pid = typeof router.query.pid === 'string' ? router.query.pid : '';
+
+  productsHooks.useProductsLoad(pid);
 
   return (
     <MainLayout title="Каталог">
