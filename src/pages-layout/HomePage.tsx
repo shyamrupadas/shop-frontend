9255ef6df9e73e5685eaab391d5e10d905c775fd
Denis Evlampiev <dev@evlampiev.com>
2022-02-24 01:26:30 +0300

    refactoring: remove ProductsList component

--- src/pages-layout/HomePage.tsx
+++ src/pages-layout/HomePage.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Container } from '@mui/material';
-import CategoryList from 'entities/category/ui/CategoryList';
+import { Container, Divider, Stack } from '@mui/material';
+import { useSelector } from 'react-redux';
+import categoriesSelector from '../entities/category/model/categoriesSelectors';
+import { useWindowWidth } from 'shared/hooks';
+import ProductsList from '../entities/product/ui/ProductsList';
+import { ProductCard } from 'widgets/product-card';
 
 const HomePage = () => {
+  const categories = useSelector(categoriesSelector.categories);
+  const rowItems = useWindowWidth();
+
   return (
     <Container maxWidth="lg">
-      <CategoryList />
+      <Stack spacing={2} mt="32px" mb="32px" divider={<Divider flexItem />}>
+        {categories.map((category) => {
+          const renderingProducts = category.products.slice(0, rowItems);
+
+          return (
+            <ProductsList key={category._id} category={category}>
+              {renderingProducts.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+            </ProductsList>
+          );
+        })}
+      </Stack>
     </Container>
   );
 };
