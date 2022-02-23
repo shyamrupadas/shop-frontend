5808a5e53f0e80f6501fac133c80611a90c45aee
Denis Evlampiev <dev@evlampiev.com>
2022-02-24 01:11:23 +0300

    feature: cart model changes

--- src/entities/catalog/model/index.ts
+++ src/entities/catalog/model/index.ts
@@ -2,3 +2,4 @@ export * from './catalog.slice';
 export * as thunks from './catalog.thunks';
 export * as selectors from './catalog.selectors';
 export * as convertPageToRows from './convertPageToRows';
+export * as hooks from './catalog.hooks';
